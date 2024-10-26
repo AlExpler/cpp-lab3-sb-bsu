c27ad50deeefb5968e8d67c82ad9ca9c2b1d615a
AlExpler <help.ftsgroup@gmail.com>
2024-10-26 12:51:04 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <cmath>
 
-int main()
-{
+
+int main() {
 int k, i = 1, sum_k = 0, number;
 while (true) {
-std::cout << " Enter the amount of integers to sum = "; std::cin >> k;
-if (k <= 0) { std::cout << "Invalid value. Please input a positive integer number\n"; continue; }
+std::cout << "Enter the amount of integers to sum = "; std::cin >> k;
+if (k <= 0) { std::cout << "Invalid value. Please, input a positive integer number.\n"; continue; }
 break;
 }
 do {
 std::cout << "Enter integer nr. " << i << ": ";
 std::cin >> number;
+if (std::cin.fail()) { std::cout << "Invalid value. Please input a valid integer number.\n"; std::cin.clear(); std::cin.ignore(10000,'\n'); continue; }
 sum_k += number; i++;
 } while (i <= k);
 std::cout << " The total sum of " << k << " integers is: " << sum_k;
